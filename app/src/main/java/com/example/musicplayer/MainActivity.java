efbc737d8e94d3304d5dad8d34358cd9d816bfe6
Vladimir <netqualityteam@gmail.com>
2021-08-01 13:05:41 +0300

    first commit

---
+++ app/src/main/java/com/example/musicplayer/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.musicplayer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+    ListView listView;
+    String[] items;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+    protected void runtimePermission(){
+
+    }
+}
